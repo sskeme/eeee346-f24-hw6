027fd5297b36f51eaea46ec5971ffbe0ac182394
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2024-10-20 18:32:47 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include "car.h"
+
+int main() {
+  string n;
+  double r;
+  double l;
+  double v;
+
+  // TODO: create an object named car1 from the tank_car class, calling the constructor without parameters
+  // TODO: store the value of car1's radius in the variable r
+  // TODO: store the value of car1's length in the variable l
+  // TODO: store the value of car1's name in the variable n
+  // TODO: calculate the volume of car1 and store it in the variable v
+
+  cout << "== Information on " << n << " ==" << endl;
+  cout << "  radius: " << r << endl;
+  cout << "  length: " << l << endl;
+  cout << "  volume: " << v << endl;
+
+  // TODO: update car1's radius to 3.4
+  // TODO: update car1's length to 10.1
+  // TODO: update car1's name to "TC1"
+
+  // TODO: store the value of car1's radius in the variable r
+  // TODO: store the value of car1's length in the variable l
+  // TODO: store the value of car1's name in the variable n
+  // TODO: calculate the volume of car1 and store it in the variable v
+
+  cout << "== Information on " << n << " ==" << endl;
+  cout << "  radius: " << r << endl;
+  cout << "  length: " << l << endl;
+  cout << "  volume: " << v << endl;
+
+  // TODO: create an object named car2 from the tank_car class, calling the constructor with parameters
+  //     : pass the arguments 5.6, 2.1, and "TC2" for radius, length, and name, respectively
+  // TODO: store the value of car2's radius in the variable r
+  // TODO: store the value of car2's length in the variable l
+  // TODO: store the value of car2's name in the variable n
+  // TODO: calculate the volume of car2 and store it in the variable v
+
+  cout << "== Information on " << n << " ==" << endl;
+  cout << "  radius: " << r << endl;
+  cout << "  length: " << l << endl;
+  cout << "  volume: " << v << endl;
+
+  return 0;
+}
